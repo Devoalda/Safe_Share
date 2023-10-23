fc4979e13f8c5df254c20e0977bd2fe0cbd2144e
Benjamin Loh <xrando20@gmail.com>
2023-10-23 14:14:05 +0800

    shifted frontend directory in app
    shifted frontend directory in app

--- safeshare/safeshare-frontend/src/index.js
+++ safeshare/safeshare-frontend/src/index.js
@@ -8,9 +8,9 @@ import HomePage from "./pages/home";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
+    // <React.StrictMode>
+    //   <App />
+    // </React.StrictMode>
     <Router>
         <Routes>
             <Route path='*' element={<HomePage/>}/>
